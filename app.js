537579892fb8909132af17882303e53d0736a84a
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2019-10-29 10:48:12 -0600

    validating that query fields must contain values

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
+const movies = require('./movies.json');
 
 const app = express();
 app.use(morgan('dev'));
@@ -21,7 +22,20 @@ app.use(function validateBearerToken(req, res, next) {
 
 
 app.get('/movie', (req, res) => {
-  return res.json('hi');
+  const filteredMovies = movies;
+  const { genre, country, avg_vote} = req.query;
+  
+  if('genre' in req.query && !req.query.genre){
+    return res.status(400).json({error: 'Genre search field must contain value'});
+  }
+  if('country' in req.query && !req.query.country){
+    return res.status(400).json({error: 'Country search field must contain value'});
+  }
+  if('avg_vote' in req.query && !req.query.avg_vote){
+    return res.status(400).json({error: 'Average Vote search field must contain value'});
+  }
+
+  return res.json(filteredMovies);
 });
 
 app.listen(8000, () => {
